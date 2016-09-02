0bf937ce58295760ae74e8103ffd0504d04817e0
James Williams <james@jamesrwilliams.co.uk>
2016-09-02 17:10:25 +0100

    Prepping gulp init
    RM duped folders. Begun documenting gulpfile.js and setting up the gulp
    init task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,21 @@
+// *************************************
+//
+//   Gulpfile
+//
+// *************************************
+//
+// Available tasks:
+//   `gulp`
+//
+//
+// -------------------------------------
+//   Modules
+// -------------------------------------
+//
+// gulp              : The streaming build system
+//
+//
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var concat = require('gulp-concat');
@@ -7,6 +25,7 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var del = require('del');
 var vinylPaths = require('vinyl-paths');
+var rename = require('gulp-rename');
 
 var paths = {
 	
@@ -15,7 +34,15 @@ var paths = {
 	
 };
 
+// -------------------------------------
+//   Task: default
+// -------------------------------------
+
 gulp.task('default', ['sass', 'js']);
+
+// -------------------------------------
+//   Task: sass
+// -------------------------------------
 
 gulp.task('sass', function(done) {
 	
@@ -32,17 +59,52 @@ gulp.task('sass', function(done) {
 	
 });
 
+// -------------------------------------
+//   Task: init
+// -------------------------------------
+
+//*
+
 gulp.task('init', function(done){
 	
-	gulp.src('./src/config.php')
-	.pipe(gulp.dest('./dist/system/expressionengine/config/'))
-	.on('end', done),
+	// TODO: Move System to parent directory
+	
+	// Update config file from src version to new one
+	
+	// TODO: Grab verison number and replace in custom version
 	
 	gulp.src('./src/config.php')
+	.pipe(gulp.dest('./dist/system/expressionengine/config/')),
+	
+	gulp.src('./src/config.php')
+	.pipe(vinylPaths(del)),
+	
+	// Rename ExpressionEngine 'images' default folder to 'assets'
+	
+	gulp.src('./dist/public_html/images/*')
+	.pipe(gulp.dest("./dist/public_html/assets/")),
+	
+	gulp.src('./dist/public_html/images/')
+	.pipe(vinylPaths(del)),
+	
+	// Move the uploads folder move to its parent directory
+	
+	gulp.src('./dist/public_html/assets/uploads/')
+	.pipe(gulp.dest('./dist/public_html/')),
+	
+	gulp.src('./dist/public_html/assets/uploads')
 	.pipe(vinylPaths(del))
 	.on('end', done);
 	
+	// TODO: Create /img/ folder within assets
+
 });
+
+// */
+
+// -------------------------------------
+//   Task: js
+// -------------------------------------
 
 gulp.task('js', function(done) {
 	
@@ -58,6 +120,10 @@ gulp.task('js', function(done) {
     .on('end', done);
 	
 });
+
+// -------------------------------------
+//   Task: watch
+// -------------------------------------
 
 gulp.task('watch', function() {
 	
